46a94b11ff91f9e62321db04d60a6312f555acc5
Nguyen Khac Tue <your-email@gmail.com>
2026-01-23 23:38:24 +0700

    update code

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+function MyButton() {
+    return ( <div>
+        <button className={styles.btn}>Click Me!</button>
+    </div> );
+}
+
+export default MyButton;
