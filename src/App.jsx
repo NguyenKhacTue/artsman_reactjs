430fd46c76216ed4e60c3e22b3a3540870702c45
Nguyen Khac Tue <your-email@gmail.com>
2026-01-25 21:36:25 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import MyFooter from '@components/Footer/Footer';
-import MyHeader from '@components/Header/Header';
-import MainLayout from '@components/Layout/Layout';
-import MyButton from '@components/Button/Button';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import routers from '@/routers/routers';
+import { Suspense } from 'react';
+import { SidebarProvider } from '@/contexts/SideBarProvider';
+import SideBar from '@components/Sidebar/Sidebar';
+import { ToastProvider } from '@/contexts/ToastProvider';
+import { StoreProvider } from '@/contexts/storeProvider';
 
 function App() {
     return (
-        <>
-            <MainLayout>
-                {/*  <MyHeader />
-                Content
-                <MyFooter />*/}
-                <MyButton />
-            </MainLayout>
-        </>
+        <StoreProvider>
+            <ToastProvider>
+                <SidebarProvider>
+                    <BrowserRouter>
+                        <SideBar />
+
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Routes>
+                                {routers.map((item, index) => {
+                                    return (
+                                        <Route
+                                            path={item.path}
+                                            element={<item.component />}
+                                            key={index}
+                                        />
+                                    );
+                                })}
+                            </Routes>
+                        </Suspense>
+                    </BrowserRouter>
+                </SidebarProvider>
+            </ToastProvider>
+        </StoreProvider>
     );
 }
 
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,25 @@
 import styles from './styles.module.scss';
+import classNames from 'classnames';
 
-function MyButton() {
-    return ( <div>
-        <button className={styles.btn}>Click Me!</button>
-    </div> );
+function Button({
+    content,
+    isPriamry = true,
+    customClassname = false,
+    ...props
+}) {
+    const { btn, primaryBtn, secondaryBtn } = styles;
+    return (
+        <button
+            className={classNames(btn, {
+                [primaryBtn]: isPriamry,
+                [secondaryBtn]: !isPriamry,
+                [customClassname]: customClassname
+            })}
+            {...props}
+        >
+            {content}
+        </button>
+    );
 }
 
-export default MyButton;
+export default Button;
